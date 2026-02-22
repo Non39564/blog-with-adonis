16b81d3f79d6e3c9eb95a932688a38ca274fd04a
non39564 <sarawut.o@ubu.ac.th>
2026-02-22 10:32:51 +0700

    feat: implement form model and database migration, update posts controller for CRUD operations

--- app/controllers/posts_controller.ts
+++ app/controllers/posts_controller.ts
@@ -1,24 +1,26 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import Form from '#models/form';
 
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-}
+// interface Post {
+//   id: number;
+//   title: string;
+//   body: string;
+// }
 
-const posts: Post[] = [
-  { id: 1, title: 'Hello AdonisJS', body: 'Adonis includes everything you need to create fully functional web app or an API server.' },
-  { id: 2, title: 'VueJS', body: 'Vue is a progressive framework for building user interfaces.' }
-]
+// const posts: Post[] = [
+//   { id: 1, title: 'Hello AdonisJS', body: 'Adonis includes everything you need to create fully functional web app or an API server.' },
+//   { id: 2, title: 'VueJS', body: 'Vue is a progressive framework for building user interfaces.' }
+// ]
 
 export default class PostsController {
   async index({ view }: HttpContext) {
+    const posts = await Form.all()
     return view.render('posts', { posts: posts })
   }
 
   async show({ params, view }: HttpContext) {
     const id = params.id
-    const post = posts.find((p) => p.id == Number(id))
+    const post = await Form.find(id)
     return view.render('detail', { post: post })
   }
 
@@ -29,30 +31,36 @@ export default class PostsController {
   async store({ request, response }: HttpContext) {
     const title = request.input('title')
     const body = request.input('body')
-    const newId = posts.length + 1
-    const newPost: Post = { id: newId, title: title, body: body }
-    posts.push(newPost)
-    return response.redirect().toPath('/posts')
+    const post = new Form()
+    post.title = title
+    post.body = body
+    await post.save()
+    return response.redirect().toRoute('posts.home')
   }
 
   async edit({ params, view }: HttpContext) {
     const id = params.id
-    const post = posts.find(p => p.id == Number(id))
+    const post = await Form.find(id)
     return view.render('form', { post: post })
   }
 
   async update({ params, request, response }: HttpContext) {
     const id = params.id
-    const index = posts.findIndex(p => p.id == Number(id))
-    posts[index].title = request.input('title')
-    posts[index].body = request.input('body')
-    return response.redirect().toPath('/posts/' + id)
+    const post = await Form.find(id)
+    if (post) {
+      post.title = request.input('title')
+      post.body = request.input('body')
+      await post.save()
+    }
+    return response.redirect().toRoute('posts.show', { id: id })
   }
 
   async destroy({ params, response }: HttpContext) {
     const id = params.id
-    const index = posts.findIndex(p => p.id == Number(id))
-    if (index !== -1) { posts.splice(index, 1) }
-    return response.redirect().toPath('/posts')
+    const post = await Form.find(id)
+    if (post) {
+      await post.delete()
+    }
+    return response.redirect().toRoute('posts.home')
   }
 }
